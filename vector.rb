7748ec7b8a6eef25a3acfd9f630d7f55cc808eff
fuugeta <fuu.begeta.sparking@gmail.com>
2017-09-26 12:12:52 +0900

    こめ

--- vector.rb
+++ vector.rb
@@ -1,7 +1,14 @@
 class Vector
+  attr_accessor :x, :y
   def initialize(x, y)
     @x = x
     @y = y
+  end
+
+  def add(v)
+    x = @x + v.x
+    y=  @y + v.y
+    v= Vector.new(x,y)
   end
 
   def to_s
@@ -12,3 +19,12 @@ class Vector
     Math.sqrt(@x**2 + @y**2)
   end
 end
+
+
+v1 = Vector.new(1, 2)
+
+v2 = Vector.new(3, 4)
+
+v3 = v1.add(v2)
+
+puts v3.to_s
